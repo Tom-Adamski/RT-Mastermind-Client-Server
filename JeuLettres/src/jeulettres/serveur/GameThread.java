1d3abce57b799e181d535e15dce6f9f6a8ad774e
Lorys-h <45313146+Lorys-h@users.noreply.github.com>
2019-03-31 21:56:05 +0200

    Update GameThread.java

--- JeuLettres/src/jeulettres/serveur/GameThread.java
+++ JeuLettres/src/jeulettres/serveur/GameThread.java
@@ -5,18 +5,22 @@ import java.net.Socket;
 
 public class GameThread extends Thread {
 	
+	// Attribute
 	private GameService gameService;
 	
+	// Constructor initialisation
 	public GameThread(Runnable target) {
 		super(target);
 		if(target instanceof GameService)
 			this.gameService = (GameService)target;
 		}
 	
+	// Get a gamer in the gameService
 	public Joueur getJoueur() {
 		return gameService.getJoueur();
 	}
 	
+	// Check the existence of the gamer pseudo in the gamerService
 	public boolean checkPseudo(String pseudo) {
 		if(gameService.getPseudo().equals(pseudo)) {
 			return true;
@@ -24,6 +28,7 @@ public class GameThread extends Thread {
 		return false;
 	}
 	
+	// Check a gamer password for a specific gamer
 	public boolean checkPassword(String password) {
 		if(gameService.getPassword().equals(password)) {
 			return true;
@@ -31,6 +36,7 @@ public class GameThread extends Thread {
 		return false;
 	}
 	
+	// Rest all gamer scores
 	public void resetScore() {
 		gameService.resetScore();
 	}
