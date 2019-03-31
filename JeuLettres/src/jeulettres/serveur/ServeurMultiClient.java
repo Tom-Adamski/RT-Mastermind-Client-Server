350bd5ced7d225ac92138e321fc831e16305e93d
Lorys-h <45313146+Lorys-h@users.noreply.github.com>
2019-03-31 22:11:42 +0200

    Update ServeurMultiClient.java

--- JeuLettres/src/jeulettres/serveur/ServeurMultiClient.java
+++ JeuLettres/src/jeulettres/serveur/ServeurMultiClient.java
@@ -16,37 +16,38 @@ import java.io.PrintWriter;
 
 public class ServeurMultiClient{
 	
+	//Attributes
 	static final int MAXJOUEURS = 10;
-	
 	static List<Joueur> joueurs;
 
+	//MAIN FUNCTION
 	public static void main(String[] args) {
+		// Variable initialisation
 		ServerSocket socketserveur;
 		joueurs = Collections.synchronizedList(new ArrayList<Joueur>());
 		
 		try {
+			//Start the server connexion on a specific port
 			socketserveur = new ServerSocket(60000);
 			ConnectionService connectionService = new ConnectionService(socketserveur);
 			Thread connectionThread = new Thread(connectionService);
 			connectionThread.start();
 			
-
+			//Initialize a scanner listenning 
 			Scanner scanner = new Scanner(System.in);
 			String line = "";
 			
+			//Game loop
 			while(!line.equals("stop")) {
+				//Get the entry of the server administator
 				line = scanner.nextLine();
 				
+				// If the administrator enter this word, it reset all the scores of all the gamers
 				if(line.equals("reset")) {
 					connectionService.resetScores();
-					System.out.println("Les scores ont été réinitialisés");
+					System.out.println("Les scores ont Ã©tÃ© rÃ©initialisÃ©s");
 				}
-				
-			}
-			
-			
+			}	
 		} catch (IOException e) {e.printStackTrace();}
 	}
 }
-
-
