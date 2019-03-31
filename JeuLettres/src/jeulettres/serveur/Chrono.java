b81d268b38c749502d8b314bc6f25d180f3e88fb
Lorys-h <45313146+Lorys-h@users.noreply.github.com>
2019-03-31 21:19:57 +0200

    Update Chrono.java

--- JeuLettres/src/jeulettres/serveur/Chrono.java
+++ JeuLettres/src/jeulettres/serveur/Chrono.java
@@ -2,16 +2,20 @@ package jeulettres.serveur;
 
 public class Chrono {
 	
+	// Attributes
 	private long tempsDebut;
 	
+	// Initialisation of the attribute
 	public Chrono() {
 		this.tempsDebut = 0;
 	}
 	
+	// Launch the chrono
 	public void start() {
 		tempsDebut = System.currentTimeMillis();
 	}
 	
+	// Get the chrono time when called
 	public long getElapsedTime() {
 		return (System.currentTimeMillis() - tempsDebut)/1000;
 	}
